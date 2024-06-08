28ed86c2d2d45dd24af63dd312b5d9bc254935c5
Daniel Neres <danielnerres@gmail.com>
2024-06-07 21:04:43 -0300

    mecanismo de destruição do projetil

--- Assets/scripts/scr_bala.cs
+++ Assets/scripts/scr_bala.cs
@@ -5,15 +5,33 @@ using UnityEngine;
 public class scr_bala : MonoBehaviour
 {
     public float velocidade;
+    private Camera cam;
 
     void Start()
     {
-        
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.right * velocidade * Time.deltaTime);
+
+        float tamanho_vertical = cam.orthographicSize;
+        float tamanho_horizontal = tamanho_vertical * cam.aspect;
+        
+        if (transform.position.x > tamanho_horizontal){
+            Destroy(gameObject);
+        }
+        else if (transform.position.x < -tamanho_horizontal){
+            Destroy(gameObject);
+        }
+
+        if (transform.position.y > tamanho_vertical){
+            Destroy(gameObject);
+        }
+        else if (transform.position.y < -tamanho_vertical){
+            Destroy(gameObject);
+        }
     }
 }
