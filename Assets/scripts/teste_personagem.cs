7d585a347b236c9504c981ea8d1e27bb790d952e
Daniel Neres <danielnerres@gmail.com>
2024-06-11 17:18:14 -0300

    quase lá

--- Assets/scripts/teste_personagem.cs
+++ Assets/scripts/teste_personagem.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class teste_personagem : MonoBehaviour
 {
-    public float velocidade, pulo;
-    private bool estar_no_chao;
+    public float velocidade, pulo, velocidade_gancho;
+    private bool estar_no_chao, gancho;
     private Vector3 posicao_apoio;
     private Rigidbody2D rigidi_personagem;
 
@@ -14,35 +14,47 @@ public class teste_personagem : MonoBehaviour
     {
         estar_no_chao = true;
         rigidi_personagem = GetComponent<Rigidbody2D>();
+        gancho = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow)){
-            transform.Translate(velocidade * Time.deltaTime * transform.right);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow)){
-            transform.Translate(velocidade * Time.deltaTime * transform.right);
-        }
-        if (Input.GetKeyDown(KeyCode.Space)){
-            rigidi_personagem.AddForce(new Vector2(0, pulo), ForceMode2D.Impulse);
+        if (!gancho){
+            if (Input.GetKey(KeyCode.RightArrow)){
+                transform.Translate(velocidade * Time.deltaTime * transform.right);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow)){
+                transform.Translate(-velocidade * Time.deltaTime * transform.right);
+            }
+            if (Input.GetKeyDown(KeyCode.Space)){
+                rigidi_personagem.AddForce(new Vector2(0, pulo), ForceMode2D.Impulse);
+                estar_no_chao = false;
+            }
+
+            if (!estar_no_chao){
+                if (Input.GetKeyDown("f")){
+                    posicao_apoio = GameObject.FindGameObjectWithTag("apoio").transform.position;
+                    Debug.Log("quadrado a x = " + posicao_apoio.x + " e y = " + posicao_apoio.y);
+                    gancho = true;
+                }
+            }
+        }else{
+            transform.position = Vector3.MoveTowards(transform.position, posicao_apoio, velocidade_gancho * Time.deltaTime);
         }
 
-        if (!estar_no_chao){
-            if (Input.GetKeyDown("f")){
-                posicao_apoio = GameObject.FindGameObjectWithTag("apoio").transform.position;
-                Debug.Log("quadrado a x = " + posicao_apoio.x + " e y = " + posicao_apoio.y);
-            }
-        }
-        //transform.position = Vector3.MoveTowards(transform.position, posicao_apoio, velocidade * Time.deltaTime);
     }
 
     private void OnCollisionEnter2D(Collision2D collision){
         if (collision.gameObject.CompareTag("chao"))
         {
             estar_no_chao = true;
-            Debug.Log("estou no chao");
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.gameObject.CompareTag("apoio"))
+        {
+            gancho = false;
         }
     }
 }
