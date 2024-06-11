f1cf2fff61166dc5e51f377be08064e2d06eedba
Daniel Neres <danielnerres@gmail.com>
2024-06-11 16:04:05 -0300

    muitos aprendizados

--- Assets/scripts/teste_personagem.cs
+++ Assets/scripts/teste_personagem.cs
@@ -7,9 +7,13 @@ public class teste_personagem : MonoBehaviour
     public float velocidade, pulo;
     private bool estar_no_chao;
     private Vector3 posicao_apoio;
+    private Rigidbody2D rigidi_personagem;
+
+    
     void Start()
     {
         estar_no_chao = true;
+        rigidi_personagem = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -22,22 +26,23 @@ public class teste_personagem : MonoBehaviour
             transform.Translate(velocidade * Time.deltaTime * transform.right);
         }
         if (Input.GetKeyDown(KeyCode.Space)){
-            transform.Translate(pulo * Time.deltaTime * transform.up);
+            rigidi_personagem.AddForce(new Vector2(0, pulo), ForceMode2D.Impulse);
         }
 
         if (!estar_no_chao){
             if (Input.GetKeyDown("f")){
                 posicao_apoio = GameObject.FindGameObjectWithTag("apoio").transform.position;
+                Debug.Log("quadrado a x = " + posicao_apoio.x + " e y = " + posicao_apoio.y);
             }
         }
-        transform.position = Vector3.MoveTowards(transform.position, posicao_apoio, velocidade * Time.deltaTime);
+        //transform.position = Vector3.MoveTowards(transform.position, posicao_apoio, velocidade * Time.deltaTime);
     }
 
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Chao"))
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("chao"))
         {
-            estar_no_chao = false;
+            estar_no_chao = true;
+            Debug.Log("estou no chao");
         }
     }
 }
