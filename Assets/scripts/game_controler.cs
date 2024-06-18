9365c3c28704e09d1fb20b4dae301fb4126daf82
Daniel Neres <danielnerres@gmail.com>
2024-06-18 14:27:23 -0300

    criação da mecanica de melhorias e criação da melhoria de projetil

--- Assets/scripts/game_controler.cs
+++ Assets/scripts/game_controler.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class game_controler : MonoBehaviour
 {
     private Camera cam;
-    private float tamanho_vertical, tamanho_horizontal;    
+    private float tamanho_vertical, tamanho_horizontal, timer, tempo_de_espera;
+    public GameObject melhorias;
     void Start()
     {
         cam = Camera.main;
         tamanho_vertical = cam.orthographicSize;
         tamanho_horizontal = tamanho_vertical * cam.aspect;
+        tamanho_vertical *= 0.85f;
+        tamanho_horizontal *= 0.85f;
+        tempo_de_espera = Random.Range(5, 10);
+        timer = tempo_de_espera + Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Time.time > timer){
+            Vector3 posicao = new Vector3(Random.Range(-tamanho_horizontal, tamanho_horizontal), Random.Range(-tamanho_vertical, tamanho_vertical), 0);
+            Instantiate(melhorias, posicao, Quaternion.identity);
+            timer += Random.Range(5f, 10f);
+        }
         
     }
 }
